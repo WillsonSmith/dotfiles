a9f0d1996e772e1f91cdb5935108cac6f73ce242
Willson Smith <me@willsonsmith.com>
2023-08-15 23:39:40 -0400

    Update indent guide colors and space char

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -24,6 +24,12 @@ require("lazy").setup({
     name = "catppuccin",
     priority = 1000,
     opts = {
+      custom_highlights = function(colors)
+        return {
+          NonText = { fg = colors.color0 },
+          Whitespace = { fg = colors.color0 },
+        }
+      end,
       integrations = {
         cmp = true,
         nvimtree = true,
@@ -35,12 +41,33 @@ require("lazy").setup({
   "github/copilot.vim",
   "folke/which-key.nvim",
   "airblade/vim-gitgutter",
+
   {
     "lukas-reineke/indent-blankline.nvim",
-    opts = {},
     config = function()
+      vim.opt.termguicolors = true
+      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
       vim.opt.list = true
-      vim.opt.listchars:append "eol:↴"
+      vim.opt.listchars:append "space:·"
+
+      require('indent_blankline').setup{
+        space_char_blankline = " ",
+        space_char_highlight_list = {"clear"},
+        char_highlight_list = {
+          "IndentBlanklineIndent1",
+          "IndentBlanklineIndent2",
+          "IndentBlanklineIndent3",
+          "IndentBlanklineIndent4",
+          "IndentBlanklineIndent5",
+          "IndentBlanklineIndent6",
+        }
+      }
     end
   },
 
