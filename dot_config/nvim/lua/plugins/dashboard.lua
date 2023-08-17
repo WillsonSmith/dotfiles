80eb968bd7565737f3b2272e17a8e6e16ab1d249
Willson Smith <me@willsonsmith.com>
2023-08-17 14:26:00 -0400

    Change dashboard

--- dot_config/nvim/lua/plugins/dashboard.lua
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -5,7 +5,20 @@ local dashboard_config = {
     "nvim-tree/nvim-web-devicons"
   },
   config = function()
-    require("dashboard").setup {}
+    require("dashboard").setup ({
+      center = {
+        {
+          icon = '',
+          icon_hl = 'group',
+          desc = 'description',
+          desc_hl = 'group',
+          key = 'shortcut key in dashboard buffer not keymap !!',
+          key_hl = 'group',
+          action = '',
+        },
+      },
+      footer = {},
+    })
   end
 }
 
