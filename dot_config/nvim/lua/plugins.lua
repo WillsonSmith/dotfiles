9c2f022319c780bc8c2a690991815ceda3a394bd
Willson Smith <me@willsonsmith.com>
2023-08-17 13:15:17 -0400

    Move indent-blankline config to its own file

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -41,38 +41,7 @@ require("lazy").setup({
   "folke/which-key.nvim",
   "airblade/vim-gitgutter",
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      vim.opt.termguicolors = true
-      -- Sets the colors for the indent lines
-      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
-      vim.opt.list = true
-      vim.opt.listchars:append "space:·"
-
-      require('indent_blankline').setup{
-        space_char_blankline = " ",
-        -- This plugin seems to overwrite the highlight group for the space character
-        -- This is a workaround to unset the highlight group
-        space_char_highlight_list = {"clear"},
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
-        }
-      }
-    end
-  },
-
+  require('plugins/indent_blankline'),
   require('plugins/lsp_config'),
   require('plugins/mason');
   require('plugins/cmp'),
@@ -83,6 +52,7 @@ require("lazy").setup({
   require('plugins/telescope'),
   require('plugins/tree'),
   require('plugins/toggle_terminal'),
+
   {
     "numtostr/comment.nvim",
     opts = {},
