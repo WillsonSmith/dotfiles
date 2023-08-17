4cbd8eb8db9f5227e88dfbc7ff8764cbf51538cc
Willson Smith <me@willsonsmith.com>
2023-08-17 14:37:20 -0400

    Move lg config so that I can manage w/ dotfiles. Add aliases.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,11 +2,15 @@ starship init fish | source
 zoxide init fish | source
 
 set -Ux EDITOR nvim
+set -Ux XDG_CONFIG_HOME $HOME/.config
 
 fish_add_path $HOME/.local/bin
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
+alias nv="nvim"
+alias lg="lazygit"
 alias python="python3"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
