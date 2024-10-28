eeff716b5c69c81653664bbadfc2f559a02c15ae
Willson Smith <me@willsonsmith.com>
2024-10-28 14:47:58 -0400

    rm tree and use oil

---
+++ nvim/.config/nvim/lua/setup/oil.lua
@@ -0,0 +1,25 @@
+return {
+  lazy = {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup {
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split"
+        },
+        view_options = {
+          show_hidden = true
+        }
+      }
+
+      -- Parent dir in current folder
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Parent dir in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end
+  }
+}
