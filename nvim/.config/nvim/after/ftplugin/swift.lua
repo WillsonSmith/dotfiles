8c1e8f6c5d57f5d99df85531125db229c4e7306c
Willson Smith <me@willsonsmith.com>
2025-07-25 17:23:41 -0400

    swift patch

--- nvim/.config/nvim/after/ftplugin/swift.lua
+++ nvim/.config/nvim/after/ftplugin/swift.lua
@@ -2,6 +2,15 @@ if vim.fn.executable("sourcekit-lsp") == 1 then
   vim.lsp.start {
     name = "Sourcekit",
     cmd = { "sourcekit-lsp" },
-    root_dir = vim.fs.dirname(vim.fs.find({ "Package.swift" }, { upward = true })[1])
+    root_dir = vim.fs.dirname(vim.fs.find({ "Package.swift" }, { upward = true })[1]),
+    settings = {
+      Swift = {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = true,
+          }
+        }
+      }
+    }
   }
 end
