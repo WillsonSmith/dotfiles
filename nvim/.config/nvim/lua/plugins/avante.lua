9e7c828d8fdeb441d672f3e2cc7aa835b0bba972
Willson Smith <me@willsonsmith.com>
2025-08-02 16:48:29 -0400

    initial avante setup
    I think `make` is a bit broken but w/e

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,36 @@
+vim.pack.add {
+  {
+    src = "https://github.com/yetone/avante.nvim",
+    name = "avante",
+  },
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+
+
+  -- optionals
+  "https://github.com/zbirenbaum/copilot.lua",
+}
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(event)
+    if event.data.kind == "install" and event.data.spec.name == "avante" then
+      local path = event.data.path
+      -- This isn't running
+      vim.fn.system({ "make" }, path)
+    end
+  end,
+})
+
+require("copilot").setup {}
+
+require("avante").setup {
+  -- provider = "ollama",
+  provider = "copilot",
+  providers = {
+    ollama = {
+      endpoint = "http://localhost:11434",
+      model = "qwen3-coder"
+    },
+  }
+}
